54b42aa32763b0b65b29349ded7bd99af1ccde2a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-05-16 10:06:13 -0700

    update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,7 +17,7 @@ export const Login = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-sm-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 ">
-                        <div className="card p-3 p-md-4">
+                        <div className="card p-3 p-md-4 shadow-lg p-3 mb-5 bg-body rounded">
                             <div className="row">
                                 <div className="col">
                                     <h1 className="text-center mb-5"> Login</h1>
